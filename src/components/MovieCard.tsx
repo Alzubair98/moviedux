151e28b8502a7e10ce0d1358e4d9400babf29b69
Alzubair98 <zubairsk53@gmail.com>
2025-10-26 09:28:46 +0300

    load default image if SM went wrong

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,9 +1,18 @@
 import type { Movie } from "../types/movie";
+import type { SyntheticEvent } from "react";
 
 export const MovieCard = ({ movie }: { movie: Movie }) => {
+  const handleError = (e: SyntheticEvent<HTMLImageElement>) => {
+    e.currentTarget.src = "images/default.jpg";
+  };
+
   return (
     <div key={movie.id} className="movie-card cursor-pointer">
-      <img src={`images/${movie.image}`} alt={movie.title} />
+      <img
+        src={`images/${movie.image}`}
+        alt={movie.title}
+        onError={handleError}
+      />
       <div className="movie-card-info">
         <h3 className="movie-card-title">{movie.title}</h3>
         <p className="movie-card-genre">{movie.genre}</p>
