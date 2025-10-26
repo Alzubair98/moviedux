86faf06eb6346f8faad89c1ef07eee856657a447
Alzubair98 <zubairsk53@gmail.com>
2025-10-26 11:32:46 +0300

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { MoviesGrid } from "./components/MoviesGrid";
