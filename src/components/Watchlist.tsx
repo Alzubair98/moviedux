ee36caa4080aae28a2321bba51ccc1fc8d2cd1e5
Alzubair98 <zubairsk53@gmail.com>
2025-10-28 14:17:33 +0300

    update watchlist and show cards

--- src/components/Watchlist.tsx
+++ src/components/Watchlist.tsx
@@ -1,4 +1,6 @@
 import type { Movie } from "../types/movie";
+import MovieCard from "./MovieCard";
+import { PageWrapper } from "./pageWrapper/PageWrapper";
 
 export const WatchList = ({
   watchlist,
@@ -6,12 +8,25 @@ export const WatchList = ({
   toggleWatchlist,
 }: {
   watchlist: number[];
-  movies: Movie;
+  movies: Movie[];
   toggleWatchlist: (movieId: number) => void;
 }) => {
   return (
     <div>
       <h1 className="title">Your Watchlist</h1>
+      <div className="watchlist">
+        {watchlist.map((id) => {
+          const movie = movies.find((movie) => movie.id === id);
+          return (
+            <MovieCard
+              key={id}
+              movie={movie}
+              toggleWatchlist={toggleWatchlist}
+              isWatchlisted={true}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
